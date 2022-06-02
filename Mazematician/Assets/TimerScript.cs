0afad81ef12a6e91d3ab909d371da0fdaf2b92b8
Andrew Greene <agreene@Andrews-MBP.home>
2022-06-02 14:30:48 -0700

    Added Timer script, but UI layer not displaying properly

---
+++ Mazematician/Assets/TimerScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    public float time = 0;
+    public Text windowText;
+    // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time < 62){
+            time += Time.deltaTime;
+        }
+        else {
+            // Text = Out of Time
+        }
+        updateTimer(time);
+    }
+    void updateTimer(float currentTime)
+    {
+        // if(currentTime < 0){
+        //     currentTime = 0;
+        // }
+        //currentTime += 1;
+        float minutes = Mathf.FloorToInt(currentTime / 60);
+        float seconds = Mathf.FloorToInt(currentTime % 60);
+        windowText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
