fac1af883b8e065ba7efad6b0aa786eee32e86e8
syedhameedmohamed <syedhame@usc.edu>
2022-06-17 21:42:22 -0700

    Added Analytics

--- Mazematician/Assets/Scripts/GameEndController.cs
+++ Mazematician/Assets/Scripts/GameEndController.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Analytics;
 using UnityEditor;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
+using System;
+
 
 public class GameEndController : MonoBehaviour
 {
     public int targetScore;
+    public int totalTime = 120;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,8 +39,41 @@ public class GameEndController : MonoBehaviour
             if (script.score == targetScore)
             {
                 // script.NotifyPlayerWin();
+                getTimeInfo();
                 SceneManager.LoadScene("GameOverWon");
             }
         }
     }
+
+    private void getTimeInfo()
+    {
+        GameObject timer = GameObject.FindGameObjectWithTag("timer");
+        string timeText = timer.GetComponent<TextMeshProUGUI>().text;
+        string[] timeFormat = timeText.Split(':');
+        int totalTimeRemaining = Mathf.FloorToInt(Int32.Parse(timeFormat[0]) * 60 + Int32.Parse(timeFormat[1]));
+        int timeTakenToCompleteLevel = totalTime - Mathf.FloorToInt(totalTimeRemaining);
+
+        Debug.Log("Time Remaining: " + totalTimeRemaining + " seconds");
+        Debug.Log("Time Taken to Complete Level: " + timeTakenToCompleteLevel + " seconds");
+
+        int score = totalTimeRemaining * 30;
+        Debug.Log("Score: " + score);
+
+        AnalyticsResult analyticsResult = Analytics.CustomEvent("LevelWin", new Dictionary<string, object>
+        {
+            {
+                "Level", "Level_0"
+            },
+            {
+                "Time To Complete Level", timeTakenToCompleteLevel
+            },
+            {
+                "Score", score
+            }
+        }) ;
+        Debug.Log("analyticsResult for Level Win: " + analyticsResult);
+        
+        
+    }
+
 }
--- Mazematician/Assets/Scripts/TimerScript.cs
+++ Mazematician/Assets/Scripts/TimerScript.cs
@@ -3,29 +3,31 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Analytics;
 using UnityEngine.UI;
 
 public class TimerScript : MonoBehaviour
 {
-    public float time = 120;
+    public float time;
     public GameObject windowText;
     // Start is called before the first frame update
     //void Start()
     //{
-    //    GameObject timerObject = Instantiate(windowText, new Vector3(379f, 17f, -10f), Quaternion.identity);
+        //GameObject timerObject = Instantiate(windowText, new Vector3(379f, 17f, -10f), Quaternion.identity);
     //}
 
     // Update is called once per frame
     void Update()
     {
         if (time > 0){
+            updateTimer(time + 1);
             time -= Time.deltaTime;
-            updateTimer(time);
-
         }
         else {
             //windowText.text = "Times Up!";
+            updateAnalytics();
             SceneManager.LoadSceneAsync(2);
+            return;
         }
     }
     void updateTimer(float currentTime)
@@ -42,7 +44,26 @@ public class TimerScript : MonoBehaviour
         float seconds = Mathf.FloorToInt(currentTime % 60);
 
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
-        Debug.Log("Timer Text: " + timerText.text);
+        //Debug.Log("Timer Text: " + timerText.text);
         //Debug.Log("Timer Text: " + windowText.GetComponent<TextMeshProUGUI>().text);
     }
+
+    void updateAnalytics()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("player");
+        int playerScore = player.GetComponent<PlayerController>().score;
+        Debug.Log("Player Score: " + playerScore);
+
+        AnalyticsResult analyticsResult = Analytics.CustomEvent("LevelLoss", new Dictionary<string, object>
+        {
+            {
+                "Level", "Level_0"
+            },
+            {
+                "Player Score during end of game", playerScore
+            }
+        });
+        Debug.Log("analyticsResult for LevelLoss: " + analyticsResult);
+    }
+
 }
