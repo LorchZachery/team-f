85025466ebb644336b2e426e9f23f6e7b1d6613e
Abhishek Venkatesh <abhishek.venkatesh2016@gmail.com>
2022-06-21 17:10:37 -0700

    removed button onclicks from other buttons

--- Mazematician/Assets/Scripts/MainMenu.cs
+++ Mazematician/Assets/Scripts/MainMenu.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-
+    // Loads levels screen
     public void PlayButton()
     {
         SceneManager.LoadScene("Levels");
