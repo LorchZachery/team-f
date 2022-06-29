1155eb8c6c9be5c212a77911e2cbda6f0f47e162
LorchZachery <lorchzachery@gmail.com>
2022-06-28 18:11:56 -0700

    fixing small error

--- Mazematician/Assets/Scripts/DashBoardController.cs
+++ Mazematician/Assets/Scripts/DashBoardController.cs
@@ -26,6 +26,7 @@ public class DashBoardController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        remainingTime = 60 * 2f;
         if(MainMenu.TutorialButtonClicked) {
             if(TutorialController.TutorialLevelName == "Tutorial_2") {
                 remainingTime = 25 * 1f;
