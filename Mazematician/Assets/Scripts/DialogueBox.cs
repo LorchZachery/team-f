efe18ab0e03a1f3b92f99edd1e4f8034368a9d9f
Jasmine Sodhi <jasmineesodhi@gmail.com>
2022-07-14 19:18:51 -0700

    prompts for level 3

--- Mazematician/Assets/Scripts/DialogueBox.cs
+++ Mazematician/Assets/Scripts/DialogueBox.cs
@@ -3,36 +3,76 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
+
+// Collect coins
+// For spike - back side 
+
+
 public class DialogueBox : MonoBehaviour
 {
-    public TextMeshProUGUI textComponent;
-    public string message;
-    public float speed;
+    public GameObject coinPrompt;
+    public GameObject shieldPrompt;
+    public GameObject shrinkPrompt;
+    public GameObject walkThruPrompt;
+    
+    GameObject player;
+    string LevelName;
 
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = string.Empty;
-        StartDialogue();
+        disableAllPrompts();
+    }
+
+    public void SetPlayer(GameObject player)
+    {
+        this.player = player;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    
-    void StartDialogue()
-    {
-        StartCoroutine(TypeLine());
+        if (player != null)
+        {
+            LevelName = LevelsController.LevelName;
+            var script = player.GetComponent<PlayerController>();
+
+            if (LevelName == "newtutorial31")
+            {
+                switch(script.score)
+                {
+                    case 4:
+                        coinPrompt.SetActive(true);
+                        StartCoroutine(closePrompt(coinPrompt));
+                        break;
+                    case 8:
+                        shieldPrompt.SetActive(true);
+                        StartCoroutine(closePrompt(shieldPrompt));
+                        break;
+                    case 16:
+                        shrinkPrompt.SetActive(true);
+                        StartCoroutine(closePrompt(shrinkPrompt));
+                        break;
+                    case 128:
+                        walkThruPrompt.SetActive(true);
+                        StartCoroutine(closePrompt(walkThruPrompt));
+                        break;
+                }
+            }
+        }
     }
 
-    IEnumerator TypeLine()
+    void disableAllPrompts()
     {
-        foreach (char c in message.ToCharArray())
-        {
-            textComponent.text += c;
-            yield return new WaitForSeconds(speed);
-        }
+        coinPrompt.SetActive(false);
+        shieldPrompt.SetActive(false);
+        shrinkPrompt.SetActive(false);
+        walkThruPrompt.SetActive(false);
+    }
+
+    IEnumerator closePrompt(GameObject obj)
+    {
+        yield return new WaitForSeconds(3.0f);
+        Destroy(obj);
     }
 }
